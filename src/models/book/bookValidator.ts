fc55da29ad92778d68a90eb593a69f9d14ea740a
Andrew Grimm <andrewgrimm2@gmail.com>
2019-11-10 07:32:25 +1300

    (FAYB-09)Test-cases-for-get-post-book - removed unused dependency and added return type to function.

--- src/models/book/bookValidator.ts
+++ src/models/book/bookValidator.ts
@@ -2,6 +2,6 @@ import Validator from 'jsonschema';
 import bookSchema from './bookSchema.json';
 
 const schema = new Validator.Validator();
-const validBook = (book: any) => schema.validate(book, bookSchema).errors.length === 0;
+const validBook = (book: any): boolean => schema.validate(book, bookSchema).errors.length === 0;
 
 export default validBook;
