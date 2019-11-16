270e2282c5aece2c37d187aedfb8ec1195d62da8
Andrew Grimm <andrewgrimm2@gmail.com>
2019-11-17 05:39:04 +1300

    FAYB-21-Create-get-all-books-endpoint - Added get books endpoint and unit tests. Added difficulty property to book model and to removed nested object to meet constraints with queries in Google Firebase.

--- src/__tests__/booksController.test.ts
+++ src/__tests__/booksController.test.ts
@@ -7,17 +7,64 @@ import { validID } from '../models/firebaseID/firebaseIDValidator';
 
 const request = supertest(app);
 
+describe('GET /books', () => {
+  afterAll((done) => {
+    app.close(done);
+  });
+
+  it('Get all books', async () => {
+    const result:any = await request.get('/books');
+    const books: [Book] = JSON.parse(result.text);
+
+    expect(result.statusCode).toEqual(200);
+
+    books.forEach((book: Book) => {
+      expect(validID(book.id)).toEqual(true);
+      expect(validBook(book)).toEqual(true);
+    });
+    expect(books.length).toBeGreaterThanOrEqual(1);
+  });
+
+  it('Get all featured books', async () => {
+    const result:any = await request.get('/books?featured=true');
+    const books: [Book] = JSON.parse(result.text);
+
+    expect(result.statusCode).toEqual(200);
+
+    books.forEach((book: Book) => {
+      expect(validID(book.id)).toEqual(true);
+      expect(validBook(book)).toEqual(true);
+      expect(book.tags).toContain('featured');
+    });
+    expect(books.length).toBeGreaterThanOrEqual(1);
+    expect(books[0].tags).toContain('featured');
+  });
+
+  it('Throw error on invalid book id', async () => {
+    const result:any = await request.get('/books/__.__');
+    expect(result.statusCode).toEqual(400);
+    expect(validErrorResponse(JSON.parse(result.text))).toEqual(true);
+  });
+
+  it('Throw error on non existant book id', async () => {
+    const result:any = await request.get('/books/0000');
+    expect(result.statusCode).toEqual(404);
+    expect(validErrorResponse(JSON.parse(result.text))).toEqual(true);
+  });
+});
+
+
 describe('GET /books/:id', () => {
   afterAll((done) => {
     app.close(done);
   });
 
   it('Get my favorite book', async () => {
-    const result:any = await request.get('/books/MMaTmwFPljQ0bV3TrLZM');
+    const result:any = await request.get('/books/88y2B3L0BbKFR3eTeC5E');
     const book: Book = JSON.parse(result.text);
     expect(result.statusCode).toEqual(200);
     expect(validID(book.id)).toEqual(true);
-    expect(book.id).toEqual('MMaTmwFPljQ0bV3TrLZM');
+    expect(book.id).toEqual('88y2B3L0BbKFR3eTeC5E');
     expect(validBook(book)).toEqual(true);
   });
 
@@ -45,12 +92,10 @@ describe('POST /books', () => {
         title: 'The Three Little Bears',
         description: 'This is the description',
         tags: ['someHashTagWithoutHash'],
-        author: {
-          name: 'Unknown',
-          contentOwnerID: 0,
-        },
+        authorUsername: 'Unknown',
         uploadDate: 0,
         language: 'eng',
+        difficulty: 1,
         cover: 'someBase64String',
         contents: ['chapter 1 Title', 'chapter 2 Title'],
         content: [
